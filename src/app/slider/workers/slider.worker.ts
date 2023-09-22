5429dfca5dbce6b01700ef95ff4e139d2666dac2
trebuszeq <trebuszeq@gmail.com>
2023-09-22 07:56:40 +0200

    22.09.23 working on slider

--- src/app/slider/workers/slider.worker.ts
+++ src/app/slider/workers/slider.worker.ts
@@ -1,27 +1,45 @@
 /// <reference lib="webworker" />
+let speed: number = 3000;
+let cleared: boolean = false;
 
 addEventListener('message', ({ data }) => {
   // const response = `worker response to ${data}`;
   // console.log(response);
-  autoSlide(data[0], data[1]);
+  autoSlide(data[0]);
+  speed = data[1];
 });
 
 
-function setSetInterval(speed: number) {
-  let interv = setInterval( 
-    () => {
-      postMessage(true);
-  }, speed);
-  return interv;
+let interval2 = setInterval(intervalHandlerSlide, speed);
+let intervalRenew = setInterval(intervalHandlerRenew, 9000);
+
+// posts message to slider service
+function intervalHandlerSlide() {
+  cleared = false;
+  postMessage(true);
 }
 
 
-function autoSlide(on: boolean, speed: number) {
-  let interval;
-  if (on) interval = setSetInterval(speed);
-  else if (!on) {
-    console.log("cleared");
-    clearInterval(interval);
-    postMessage(false)
+function intervalHandlerRenew() {
+  if(cleared) {
+    // console.log("Slider renewed");
+    interval2 = setInterval(intervalHandlerSlide, speed);
+  }
+}
+
+
+// controls auto sliding
+function autoSlide(on: boolean) {
+  switch(on) {
+    case false:
+      // console.log("cleared");
+      clearInterval(interval2);
+      cleared = true;
+      postMessage(false);
+      break;
+
+    case true:
+      interval2 = setInterval(intervalHandlerSlide, speed);
+      break;
   }
 }
