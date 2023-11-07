54d030b1623940024846955c4c0d49597c57451e
TrebuszeQ <trebuszeq@gmail.com>
2023-11-08 00:06:39 +0100

    07.11.23 Angular http is exhausting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 // misc
@@ -31,8 +32,12 @@ import { GalleryComponent } from './gallery/gallery.component';
     AppRoutingModule,
     FontAwesomeModule,
     NgOptimizedImage,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(),
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/galleries.service.ts
+++ src/app/services/galleries.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 // classess, interfaces
 import { GalleryImage } from '../classes/gallery-image';
+import { HttpClient, HttpRequest } from '@angular/common/http';
+import { Config } from '@fortawesome/fontawesome-svg-core';
 
 @Injectable({
   providedIn: 'root'
@@ -9,29 +11,57 @@ export class GalleriesService {
   private readonly Galleries: Map<string, GalleryImage[]> = new Map();
   private readonly BaseUrl;
   // in the future it can fetch galleries from db
-  constructor()
+  constructor(private http: HttpClient)
   {
     this.Galleries.set("gallery1", this.generateGallery("gallery1") || []);
     this.BaseUrl = "placeholderurl";
   }
 
+  // private prepareRequest(galleryKey: string) {
+  //   const url = this.BaseUrl + galleryKey;
+  //   const headers = new Headers();
+  //   headers.append("content-type", "image/jpeg");
+  //   const options: RequestInit = {
+  //     method: "GET",
+  //     headers: headers,
+  //     mode: "cors",
+  //     cache: "default",
+  //   };
+  //   return new Request(url, options);
+  // }
 
-  private fetchGalleryNames() {
-    const headers = new Headers();
-    headers.append("content-type", "text/plain");
+  // private fetchGalleryNames(galleryKey: string) {
+  //   const url = this.BaseUrl + galleryKey;
+  //   const headers = new Headers();
+  //   headers.append("content-type", "text/plain");
+    
+  // }
+
+
+  private getGalleryNames(galleryKey: string) {
+    let resBody = null;
+    this.http.get<Config>(this.BaseUrl + galleryKey, {
+      params: {},
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Cache-Control": "no-cache",
+        "X-Debug-Level": "verbose",
+      },
+      responseType: "json",
+      withCredentials: false,
+      observe: "response"
+    }).subscribe(response => {
+      resBody = response.body;
+      console.log("Response status:", response.status);
+      console.log("Body:", response.body);
+    });
+    
+    return resBody;
   }
 
-  private prepareRequest(galleryKey: string) {
-    const url = this.BaseUrl + galleryKey;
-    const headers = new Headers();
-    headers.append("content-type", "image/jpeg");
-    const options: RequestInit = {
-      method: "GET",
-      headers: headers,
-      mode: "cors",
-      cache: "default",
-    };
-    return new Request(url, options);
+  private getGalleryImages() {
+    
   }
 
   private generateGallery(galleryKey: string) {
